b4e38d44426b80e88326b82f771df8af64863559
Fabien <fabien.robin@hotmail.fr>
2016-12-24 15:28:14 +0100

    Ajout menu networking + prefabs Therapeute et Patient + déplacements et caméra

---
+++ PRED/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform playerTransform;
+	public int depth = 0;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(playerTransform != null)
+		{
+			transform.position = playerTransform.position + new Vector3(0,0.5f,depth);
+			transform.rotation = playerTransform.rotation;
+		}
+	}
+
+	public void setTarget(Transform target)
+	{
+		playerTransform = target;
+	}
+}
