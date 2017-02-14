87811a49d7066d8296b2d2b532950af3ab73b1a7
e123645x <elouarn.laine@etu.univ-nantes.fr>
2017-02-14 21:59:24 +0100

    fix for light intensity bug server side

--- PRED/Assets/Scripts/Fading.cs
+++ PRED/Assets/Scripts/Fading.cs
@@ -111,6 +111,7 @@ public class Fading : MonoBehaviour {
 
     void OnGUI()
     {
+        // computing of the transparency of the fading texture
         alpha += fadeDir * fadeSpeed * Time.deltaTime;
 		// Fading volume
 		if (SceneManager.GetActiveScene ().name == "HomeRoom" && exitingHomeRoom) {
@@ -130,9 +131,9 @@ public class Fading : MonoBehaviour {
         // clamp between 0 and 1
         alpha = Mathf.Clamp01(alpha);
         // set Color of GUI
-        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha); // transparency of the fadingTexture that will be drawn
         GUI.depth = drawDepth;
-        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), currentFadingTex);
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), currentFadingTex); // we draw the fading texture on the GUI
     }
 
     public float BeginFade ( int direction )
@@ -154,9 +155,4 @@ public class Fading : MonoBehaviour {
         alpha = 0.0f;
         fadeDir = 1;
     }
-
-    /*void OnLevelWasLoaded()
-    {
-        BeginFade(-1);
-    }*/
 }
